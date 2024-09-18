c3454bf375d61ff0f52c3b63527a3a59e83022db
rjrocha19 <rodolfo_rj@live.com>
2024-09-18 16:19:08 -0300

    add components

--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,14 @@
-import { Plus } from 'lucide-react'
-import letsStart from './assets/lets-start-illustration.svg'
-import logo from './assets/logo-in-orbit.svg'
-import { Button } from './components/ui/button.tsx'
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from './components/ui/dialog.tsx'
+import { CreateGoal } from './components/create-goal.tsx'
+import { Summary } from './components/summary.tsx'
+// import { EmptyGoals } from './components/empty-goals.tsx'
+import { Dialog } from './components/ui/dialog.tsx'
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in.orbit logo" />
-        <img src={letsStart} alt="lets start illustration" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
-          mesmo?
-        </p>
-
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar Meta
-          </Button>
-        </DialogTrigger>
-
-        <DialogContent>
-          <div>
-            <p />
-          </div>
-        </DialogContent>
-      </div>
+      {/*<EmptyGoals />*/}
+      <Summary />
+      <CreateGoal />
     </Dialog>
   )
 }
